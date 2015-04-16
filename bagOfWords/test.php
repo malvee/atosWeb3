dc009b6351f75f4aa4a1f2d9f587006602a47162
Momshad Alvee <momshadalvee@gmail.com>
2015-04-16 23:28:30 +0100

    tabs added

---
+++ bagOfWords/test.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="../twemoji/twemoji.min.js"></script>
+<head>
+<body>
+
+	<?php
+	$array = array();
+	$x = 10;
+	array_push($array, (string)$x);
+	echo $array[0];
+	?>
+
+<script>
+
+   										
+   										
+   										
+
+ </script>
+</body>
+</html>
